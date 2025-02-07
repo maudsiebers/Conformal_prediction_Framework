52a8f88585d0a65c669768c8696be3af51afd310
Maud Siebers <99180843+maudsiebers@users.noreply.github.com>
2025-02-07 15:40:28 +0000

    Add files via upload

--- Conformal_prediction_code.R
+++ Conformal_prediction_code.R
@@ -3,7 +3,7 @@
 ###### Last modified: 5-02-2025 #######
 
 
-#### example with Esthwaite Water UK with Polymer AC
+#### example with Estwaithe Water UK with Polymer AC
 
 ###### packages #######
 library(sf)
@@ -25,8 +25,9 @@ library(ggplot2)
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))  
 
 #### read in MSI estimate file and add a year column
-Chl_MSI<- read.csv('Chl_MSI_EsthwaiteWater.csv') %>% 
-  mutate(Year = year(as.Date(Date_MSI, format = '%d/%m/%Y')))
+Chl_MSI<- read.csv('Esth_Amended.csv') %>% 
+  mutate(Year = year(as.Date(Date, format = '%d/%m/%Y')),
+         Date = as.Date(Date, format = '%d/%m/%Y'))
 
 
 ###### Observed available data 
@@ -36,14 +37,14 @@ Observed <- read.csv('Observed_chl_EsthwaiteWater.csv') %>%
     Date = as.Date(Date, format = '%d/%m/%Y'),
     Observed = as.numeric(Observed),
     doy = yday(Date),
-    year = year(Date)
+    Year = year(Date)
   )
 
 
 #### non-conformity score formula 
 non_conformity_scores <- function(df_conform, df_test,df_Observed) {
-  y_conf <- df_conform$MSI_chl
-  y_conf_pred <- df_conform$Matchup
+  y_conf <- df_conform$MSI_chl 
+  y_conf_pred <- df_conform$Observed
   alpha_conf <- abs(y_conf - y_conf_pred)
   
   alpha_test <- c()
@@ -57,7 +58,7 @@ non_conformity_scores <- function(df_conform, df_test,df_Observed) {
     if (df_test$Source[i] == 'Matchup') {
       alpha_test <- c(alpha_test, abs(y_test - y_test_pred))
     } else {
-      obs_list <- df_Observed[c('Observed', 'doy', 'year')]
+      obs_list <- df_Observed[c('Observed', 'doy', 'Year')]
       differ <- abs(df_test$doy[i] - obs_list$doy)
       lowest_indices <- order(differ)[1:3]
       
@@ -65,7 +66,7 @@ non_conformity_scores <- function(df_conform, df_test,df_Observed) {
       values <- c()
       
       for (z_obs in lowest_indices) {
-        if (df_Observed$year[z_obs] == df_test$Year[i]) {
+        if (df_Observed$Year[z_obs] == df_test$Year[i]) {
           y_adjusted <- df_Observed$Observed[z_obs]
           values <- c(values, rep(y_adjusted, matching_multiplier))
         } else {
@@ -82,48 +83,54 @@ non_conformity_scores <- function(df_conform, df_test,df_Observed) {
 }
 
 
-
 ###################################################
-### loop through conformal prediction ####
+### Loop through different years of test data ###
 
 
-# Extract years
-years_list <- c(unique(Chl_MSI$Year))
-Total <- data.frame(iteration = NULL,Date = NULL, Observed = NULL,
-                    
+# Extract unique years in the dataset to loop through
+years_list <- c(unique(Chl_MSI$Year))  
+
+# Set up final dataframe 
+Conformal_output <- data.frame(iteration = NULL,Date = NULL, Observed = NULL,
                     Source = NULL,MSI_chl = NULL,conformal = NULL,
-                    upper = NULL, lower = NULL, uncertainty = NULL, 
-                    uncertainty_diff = NULL)
+                    upper = NULL, lower = NULL, uncertainty = NULL)
 
-test_year = 2017
+
+### iteration loop####
+
 
 for (test_year in years_list){
   ## test data = 1 year (test_year) + all no observation data
-
-    test_data <- Chl_MSI %>%
-      filter(Year == test_year | is.na(Date_observed)) %>%
-      mutate(Source = ifelse(!is.na(Date_observed),'Matchup', 'No Matchup'))
+    Chl_MSI_merged <- merge(Observed, Chl_MSI, by = c("Date", 'Year'), all = TRUE)
+    test_data <- Chl_MSI_merged %>%
+      filter(Year == test_year | is.na(Observed)) %>%
+      mutate(Source = ifelse(!is.na(Observed),'Matchup', 'No Matchup'))
 
   ### conformity data  - all years - test_year 
-  conformity_data <- Chl_MSI %>%
+  conformity_data <- Chl_MSI_merged %>%
     na.omit() %>% 
     filter(Year != test_year)
   
   # #### calculation of conformity scores
-  test_data$doy <- yday(test_data$Date_MSI)   # day of year calculation
-  conformity_data$doy <- yday(conformity_data$Date_observed) # day of year calculation
+  test_data$doy <- yday(test_data$Date)   # day of year calculation
+  conformity_data$doy <- yday(conformity_data$Date) # day of year calculation
 
-  results <- non_conformity_scores(conformity_data, test_data, Observed)
   
+  results <- non_conformity_scores(conformity_data, test_data, Observed) # output of non-conformity score calculation
   
-  final <- data.frame(Date = test_data$Date_MSI, Observed = test_data$Matchup, OWT =test_data$OWT, Source = test_data$Source,
+  ### Temporary file to keep data stored throughout loop
+  temp_file <- data.frame(Date = test_data$Date, Observed = test_data$Observed, OWT =test_data$OWT, Source = test_data$Source,
                       iteration = test_year)
-  # 
   
+  ## extraction of conformity scores from 'results' file and combining with conformtity file
   conf_alpha <- conformity_data
   conf_alpha$alpha <- results$alpha_conf
+  
+  # sort non-conformity scores (conformity dataset)
   conf_alpha_sorted <- arrange(conf_alpha, alpha)
-  epsilon <- 0.10
+  epsilon <- 0.05 #set threshold 
+  
+  #determine which points in conformity set fall outside of confidence interval
   qhat <- quantile(conf_alpha_sorted$alpha, 1 - epsilon, type =1)
   for (i in 1:length(conf_alpha_sorted$alpha)){
     if (conf_alpha_sorted$alpha[[i]] <= qhat){
@@ -132,70 +139,68 @@ for (test_year in years_list){
       conf_alpha_sorted$conformal[[i]] <- 'No'
     }
   }
-  
+ 
+  ## extraction of test scores from 'results' file and combining with test file
   test_alpha <- test_data
   test_alpha$alpha <- results$alpha_test
+  
+  #relating the alpha conformity to the alpha test
   
   for (i in 1:length(test_data$MSI_chl)){
     
     Y_test_pred <- test_data$MSI_chl[[i]]
-    final$MSI_chl[[i]] <- Y_test_pred
+    temp_file$MSI_chl[[i]] <- Y_test_pred
     
     alpha_test <- results$alpha_test[[i]]
-    final$alpha_test[[i]] <- alpha_test
+    temp_file$alpha_test[[i]] <- alpha_test
     alpha_conf <- conf_alpha_sorted$alpha
-    ### determine which alpha to use 
+    ### determine the position of the alpha_test in sorted alpha_confs
     pos <- findInterval(alpha_test, alpha_conf)
-    final$position[[i]]<-  as.numeric(pos)
-    final$quantile[[i]] <- as.numeric(pos/(length(conf_alpha_sorted$alpha)))
-    final$conformal[[i]] <- ifelse(alpha_test <= qhat, 'Yes','No')
+    temp_file$position[[i]]<-  as.numeric(pos)
+    #calculate its exact quatile based on the position
+    temp_file$quantile[[i]] <- as.numeric(pos/(length(conf_alpha_sorted$alpha)))
+    #determin if this is conformal or not 
+    temp_file$conformal[[i]] <- ifelse(alpha_test <= qhat, 'Yes','No')
     # use quantiles from calibration estimates as baseline intervals
     p_value <- sum(alpha_conf >= alpha_test)/(length(alpha_conf)+1)
-    final$p[[i]] <- as.numeric(p_value)
+    temp_file$p[[i]] <- as.numeric(p_value)
     
-    epsilon <- 0.05
-    
+    #upper and lower quantile 
     lower_quantile <- quantile(alpha_conf, epsilon)
     
     upper_quantile <- quantile(alpha_conf, 1 - epsilon)
     
-    
+    ### scale the prediction intervals
     median_abs_deviation <- median(alpha_conf) 
-    
     scale_factor <- alpha_test / median_abs_deviation
-    
     adjusted_lower_quantile <- lower_quantile * scale_factor
-    
     adjusted_upper_quantile <- upper_quantile * scale_factor
     
+    #calculate intervals
     int2 <- (adjusted_upper_quantile - adjusted_lower_quantile)/2
-    interval <- c(max(0,final$MSI_chl[[i]] - int2), final$MSI_chl[[i]] + int2)
+    interval <- c(max(0,temp_file$MSI_chl[[i]] - int2), temp_file$MSI_chl[[i]] + int2)
     
-    final$upper[[i]] <- as.numeric(interval[[2]])
-    final$lower[[i]] <- as.numeric(interval[[1]])
+    temp_file$upper[[i]] <- as.numeric(interval[[2]])
+    temp_file$lower[[i]] <- as.numeric(interval[[1]])
   }
   
-  final$uncertainty <- as.numeric(final$quantile) * 100
-  final$conformal <- factor(as.character(final$conformal), levels = c("Yes", "No"))
-  ### calculate the uncertainty
-  final$width <- (as.numeric(final$upper) - as.numeric(final$lower))
-  final$uncertainty_diff <- ((final$width/2)/as.numeric(final$MSI_chl))*100
+  #non-conformtiy scores to uncertainty percentages
+  temp_file$uncertainty <- as.numeric(temp_file$quantile) * 100
+  
+  #For easy of reading put a Yes No classification on 
+  temp_file$conformal <- factor(as.character(temp_file$conformal), levels = c("Yes", "No"))
+
   
   
+  temp_file <- temp_file %>% dplyr::select(iteration,Date, Observed,Source,MSI_chl,conformal,upper, lower, uncertainty, alpha_test)
   
-  
-  
-  
-  final <- final %>% dplyr::select(iteration,Date, Observed,Source,MSI_chl,conformal,upper, lower, uncertainty, uncertainty_diff,alpha_test)
-  
-  Total <- rbind(Total, final)
+  Conformal_output <- rbind(Conformal_output, temp_file)
   
 }
 
 
-Total$conformal <- factor(as.character(Total$conformal), levels = c("Yes", "No"))
-
-ggplot(data = Total, aes(x = as.Date(Date, format = '%d/%m/%Y'), y = as.numeric(MSI_chl))) +
+### plot the different iterations seperately 
+ggplot(data = Conformal_output, aes(x = as.Date(Date, format = '%d/%m/%Y'), y = as.numeric(MSI_chl))) +
   geom_point(aes(col = uncertainty, pch = conformal), size = 3) +
   geom_errorbar(
     aes(ymin = as.numeric(lower), ymax = as.numeric(upper)),
@@ -209,44 +214,13 @@ ggplot(data = Total, aes(x = as.Date(Date, format = '%d/%m/%Y'), y = as.numeric(
   facet_grid(iteration~conformal)+
   ylim(0,100)+
   scale_color_gradientn(colours = brewer.pal(9, 'OrRd'), limits = c(0, 100), na.value = "grey")+
-  ggtitle('Conformal prediciton')+
+  ggtitle('Conformal prediciton per iteration')+
   labs(color = "Conformity", pch = 'Accepted values')
-
-
-
-conformal_counts <- Total %>%
-  group_by(Date, Observed, Source, MSI_chl, conformal,alpha_test) %>%
-  summarise(count = n(), .groups = 'drop') %>%
-  pivot_wider(
-    names_from = conformal,
-    values_from = count,
-    values_fill = list(count = 0))  
-
-conformal_counts <- conformal_counts %>% mutate(Conformal_percentage = (Yes/(Yes+No)*100))
-
-Total_filter <- Total %>%
-  group_by(Date, Observed,Source,MSI_chl, alpha_test) %>%
-  summarise(
-    upper = mean(as.numeric(upper), na.rm = TRUE),
-    lower = mean(as.numeric(lower), na.rm = TRUE),
-    uncertainty = mean(as.numeric(uncertainty), na.rm = TRUE),
-    uncertainty_diff = mean(as.numeric(uncertainty_diff), na.rm = TRUE),
-    .groups = 'drop')
-
-Total_filter <- Total_filter %>%
-  left_join(conformal_counts[c("Date", "Observed", "Source", "MSI_chl",'Conformal_percentage')], 
-            by = c("Date", "Observed", "Source", "MSI_chl")) 
-
-Total_filter<- Total_filter%>% 
-  mutate(conformal = ifelse(Conformal_percentage > 90, 'Yes', 'No'))
-
-
-
 
 ######## plotting of points with their uncertainty metrics 
 
-Total_filter$conformal_2 <- ifelse(Total_filter$conformal == 'Yes', 'Accepted values', 'Rejected values')
-ggplot(data = Total_filter, aes(x = as.Date(Date, format = '%d/%m/%Y'), y = as.numeric(MSI_chl))) +
+Conformal_output$conformal_2 <- ifelse(Conformal_ouput$conformal == 'Yes', 'Accepted values', 'Rejected values')
+ggplot(data = Conformal_ouput, aes(x = as.Date(Date, format = '%d/%m/%Y'), y = as.numeric(MSI_chl))) +
   # Observed line and points with linetype mapping for the legend
   geom_line(data = Observed, aes(x = Date, y = Observed, linetype = "In situ chl-a"), color = "black") +
   geom_point(data = Observed, aes(x = Date, y = Observed), shape = 16, color = "black", show.legend = TRUE) +  # Use shape for In situ chl-a
@@ -286,11 +260,14 @@ ggplot(data = Total_filter, aes(x = as.Date(Date, format = '%d/%m/%Y'), y = as.n
 
 
 ########### Write a CSV file with the conformal outputs 
-# writing_file <- Total_filter
-# writing_file$Prediction <- as.numeric(writing_file$MSI_chl)
-# writing_file$alpha_test <- as.numeric(writing_file$alpha_test)
-# 
-# write_csv(writing_file, paste0('Conformal_output_iterative.csv'))
+writing_file <- transform(Conformal_output, 
+                          MSI_chl = as.numeric(MSI_chl), 
+                          alpha_test = as.numeric(alpha_test), 
+                          upper = as.numeric(upper), 
+                        lower = as.numeric(lower))
+
+
+write_csv(writing_file, paste0('Conformal_output_iterative.csv'))
 
 
 
